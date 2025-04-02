99117d921b5fdfb91360578740b68b2927bda990
AIIBRAMA <jurisbrama@gmail.com>
2025-04-02 11:29:06 +0300

    Update app.py

--- app.py
+++ app.py
@@ -95,83 +95,83 @@ Jūs esat eksperts Latvijas Ministru kabineta noteikumos un COFOG (Classificatio
 
 # Budžeta funkcionālo kategoriju kodu rokasgrāmata
 
-## 01.000 - Vispārējie valdības dienesti
-### 01.100 - Izpildvara, likumdošanas vara, finanšu un fiskālā darbība, ārlietas
-- **01.110** - Izpildvaras un likumdošanas varas institūcijas
-- **01.120** - Finanšu un fiskālā darbība
-- **01.130** - Ārlietas
+01.000 - Vispārējie valdības dienesti
+01.100 - Izpildvara, likumdošanas vara, finanšu un fiskālā darbība, ārlietas
+01.110 - Izpildvaras un likumdošanas varas institūcijas
+01.120 - Finanšu un fiskālā darbība
+01.130 - Ārlietas
 
-### 01.700 - Vispārējās valdības sektora parāda darījumi
-- **01.710** - Valdības valsts parāda darījumi
-- **01.720** - Pašvaldību budžetu parāda darījumi
+01.700 - Vispārējās valdības sektora parāda darījumi
+01.710 - Valdības valsts parāda darījumi
+01.720 - Pašvaldību budžetu parāda darījumi
 
-## 03.000 - Sabiedriskā kārtība un drošība
-- **03.100** - Policija
-- **03.200** - Ugunsdrošības, ugunsdzēsības, glābšanas un civilās drošības dienesti
-- **03.300** - Tiesa un prokuratūras iestādes
-- **03.400** - Ieslodzījuma vietas
+03.000 - Sabiedriskā kārtība un drošība
+03.100 - Policija
+03.200 - Ugunsdrošības, ugunsdzēsības, glābšanas un civilās drošības dienesti
+03.300 - Tiesa un prokuratūras iestādes
+03.400 - Ieslodzījuma vietas
 
-## 04.000 - Ekonomiskā darbība
-- **04.210** - Lauksaimniecība
-- **04.500** - Transports
-- **04.510** - Autotransports
+04.000 - Ekonomiskā darbība
+04.210 - Lauksaimniecība
+04.500 - Transports
+04.510 - Autotransports
 
-## 05.000 - Vides aizsardzība
-- **05.100** - Atkritumu apsaimniekošana
-- **05.200** - Notekūdeņu apsaimniekošana
-- **05.300** - Vides piesārņojuma novēršana un samazināšana
+05.000 - Vides aizsardzība
+05.100 - Atkritumu apsaimniekošana
+05.200 - Notekūdeņu apsaimniekošana
+05.300 - Vides piesārņojuma novēršana un samazināšana
 
-## 06.000 - Teritoriju un mājokļu apsaimniekošana
-- **06.100** - Mājokļu attīstība
-- **06.200** - Teritoriju attīstība
-- **06.300** - Ūdensapgāde
-- **06.400** - Ielu apgaismošana
+06.000 - Teritoriju un mājokļu apsaimniekošana
+06.100 - Mājokļu attīstība
+06.200 - Teritoriju attīstība
+06.300 - Ūdensapgāde
+06.400 - Ielu apgaismošana
 
-## 07.000 - Veselība
-- **07.100** - Ārstniecības līdzekļi
-- **07.200** - Ambulatoro ārstniecības iestāžu darbība un pakalpojumi
-- **07.300** - Slimnīcu pakalpojumi. Mātes un bērna veselības aprūpes pakalpojumi
-- **07.400** - Sabiedrības veselības dienestu pakalpojumi
+07.000 - Veselība
+07.100 - Ārstniecības līdzekļi
+07.200 - Ambulatoro ārstniecības iestāžu darbība un pakalpojumi
+07.300 - Slimnīcu pakalpojumi. Mātes un bērna veselības aprūpes pakalpojumi
+07.400 - Sabiedrības veselības dienestu pakalpojumi
 
-## 08.000 - Atpūta, kultūra un reliģija
-- **08.100** - Atpūtas un sporta pasākumi
-- **08.200** - Kultūra
-- **08.210** - Bibliotēkas
-- **08.220** - Muzeji un izstādes
-- **08.230** - Kultūras centri, nami, klubi
-- **08.300** - Apraides un izdevniecības pakalpojumi
+08.000 - Atpūta, kultūra un reliģija
+08.100 - Atpūtas un sporta pasākumi
+08.200 - Kultūra
+08.210 - Bibliotēkas
+08.220 - Muzeji un izstādes
+08.230 - Kultūras centri, nami, klubi
+08.300 - Apraides un izdevniecības pakalpojumi
 
-## 09.000 - Izglītība
-- **09.100** - Pirmsskolas izglītība (ISCED-97 0.līmenis)
-- **09.200** - Pamatizglītība, vispārējā un profesionālā izglītība (ISCED-97 1., 2. un 3.līmenis)
-- **09.210** - Vispārējā izglītība. Pamatizglītība (ISCED-97 1., 2. un 3.līmenis)
-- **09.211** - Sākumskolas (ISCED-97 1.līmenis vai tā daļa)
-- **09.220** - Profesionālā izglītība (ISCED-97 2. un 3.līmenis)
-- **09.300** - Pēcvidējā (neaugstākā) izglītība (ISCED-97 4.līmenis)
-- **09.400** - Augstākā (terciārā) izglītība (ISCED-97 5. un 6.līmenis)
-- **09.500** - Līmeņos nedefinētā izglītība
-- **09.510** - Interešu un profesionālās ievirzes izglītība
+09.000 - Izglītība
+09.100 - Pirmsskolas izglītība (ISCED-97 0.līmenis)
+09.200 - Pamatizglītība, vispārējā un profesionālā izglītība (ISCED-97 1., 2. un 3.līmenis)
+09.210 - Vispārējā izglītība. Pamatizglītība (ISCED-97 1., 2. un 3.līmenis)
+09.211 - Sākumskolas (ISCED-97 1.līmenis vai tā daļa)
+09.220 - Profesionālā izglītība (ISCED-97 2. un 3.līmenis)
+09.300 - Pēcvidējā (neaugstākā) izglītība (ISCED-97 4.līmenis)
+09.400 - Augstākā (terciārā) izglītība (ISCED-97 5. un 6.līmenis)
+09.500 - Līmeņos nedefinētā izglītība
+09.510 - Interešu un profesionālās ievirzes izglītība
 
-### 09.600 - Izglītības papildu pakalpojumi
-- **09.610** - Izglītojamo pārvadājumu pakalpojumi
-  - Uzskaita: Izdevumus par izglītojamo pārvadājumu pakalpojumiem.
-  - Neuzskaita: Braukšanas maksas atvieglojumus izglītojamiem sabiedriskā transporta maršrutu tīklā (04.500). Izdevumus degvielas kompensācijām par izglītojamā pārvadājumiem ar personīgo transportu.
-- **09.620** - Izglītojamo ēdināšanas pakalpojumi
-  - Uzskaita: Izdevumus par izglītojamo ēdināšanas pakalpojumiem.
-- **09.630** - Izglītojamo izmitināšanas pakalpojumi
-  - Uzskaita: Izdevumus par izglītojamo izmitināšanas pakalpojumiem.
-- **09.640** - Izglītojamo pārējie papildu pakalpojumi
-  - Uzskaita: Pārējos papildu pakalpojumus - vadību, pārbaudi, darbību vai atbalstu izglītojamo medicīniskajai un stomatoloģiskajai aprūpei un pārējiem pakārtotajiem pakalpojumiem, kas paredzēti neatkarīgi no izglītības līmeņa.
-  - Piezīme: Pakalpojumus uzskaita neatkarīgi no tā, vai tie sniegti izglītības iestādē autonomi vai tiek organizēts ārpakalpojums.
+09.600 - Izglītības papildu pakalpojumi
+09.610 - Izglītojamo pārvadājumu pakalpojumi
+  Uzskaita: Izdevumus par izglītojamo pārvadājumu pakalpojumiem.
+  Neuzskaita: Braukšanas maksas atvieglojumus izglītojamiem sabiedriskā transporta maršrutu tīklā (04.500). Izdevumus degvielas kompensācijām par izglītojamā pārvadājumiem ar personīgo transportu.
+09.620 - Izglītojamo ēdināšanas pakalpojumi
+  Uzskaita: Izdevumus par izglītojamo ēdināšanas pakalpojumiem.
+09.630 - Izglītojamo izmitināšanas pakalpojumi
+  Uzskaita: Izdevumus par izglītojamo izmitināšanas pakalpojumiem.
+09.640 - Izglītojamo pārējie papildu pakalpojumi
+  Uzskaita: Pārējos papildu pakalpojumus - vadību, pārbaudi, darbību vai atbalstu izglītojamo medicīniskajai un stomatoloģiskajai aprūpei un pārējiem pakārtotajiem pakalpojumiem, kas paredzēti neatkarīgi no izglītības līmeņa.
+  Piezīme: Pakalpojumus uzskaita neatkarīgi no tā, vai tie sniegti izglītības iestādē autonomi vai tiek organizēts ārpakalpojums.
 
-## 10.000 - Sociālā aizsardzība
-- **10.100** - Sociālā aizsardzība darbnespējas gadījumā
-- **10.120** - Sociālā aizsardzība invaliditātes gadījumā
-- **10.200** - Atbalsts gados veciem cilvēkiem
-- **10.400** - Atbalsts ģimenēm ar bērniem
-- **10.500** - Atbalsts bezdarba gadījumā
-- **10.600** - Mājokļa atbalsts
-- **10.700** - Pārējais citur neklasificēts atbalsts sociāli atstumtām personām
+10.000 - Sociālā aizsardzība
+10.100 - Sociālā aizsardzība darbnespējas gadījumā
+10.120 - Sociālā aizsardzība invaliditātes gadījumā
+10.200 - Atbalsts gados veciem cilvēkiem
+10.400 - Atbalsts ģimenēm ar bērniem
+10.500 - Atbalsts bezdarba gadījumā
+10.600 - Mājokļa atbalsts
+10.700 - Pārējais citur neklasificēts atbalsts sociāli atstumtām personām
 
 # Specifiskie norādījumi
 - Kad tiek prasīts kods, sniedziet TIKAI precīzu kodu un minimālu aprakstu
@@ -181,7 +181,6 @@ Jūs esat eksperts Latvijas Ministru kabineta noteikumos un COFOG (Classificatio
 # Vispārīgiem jautājumiem
 - Ja jautājums ir pārāk vispārīgs (piemēram, "ko tu zini?", "kas tu esi?"), atbildiet ar šo tekstu:
 """ + GENERIC_ANSWER
-
 
 # Funkcija teksta fragmentu meklēšanai, pamatojoties uz jautājuma kontekstu
 def search_in_text_files(query):
